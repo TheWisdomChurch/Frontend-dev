7722a7350ef1cef16df950d3fcf5368d5a7b7acd
Ogba Peter Chima <peter@skilledup.life>
2025-10-20 03:44:00 +0100

    quickFix

---
+++ components/ui/footer/Footer.tsx
@@ -0,0 +1,9 @@
+'use-cient';
+
+export default function Footer() {
+  return (
+    <div>
+      <h1> This is my footer</h1>
+    </div>
+  );
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,14 @@
-// app/layout.tsx
 import type { Metadata } from 'next';
-
 import { ThemeProvider } from '../../contexts/ThemeContext';
 import { bricolageGrotesque } from '../../lib/fonts/fonts';
-
 import './globals.css';
 
 export const metadata: Metadata = {
   title: 'Wisdom House',
   description: 'Wisdom House frontend web app',
+  icons: {
+    icon: '/favicon.ico', // 👈 update path or filename here
+  },
 };
 
 export default function RootLayout({
