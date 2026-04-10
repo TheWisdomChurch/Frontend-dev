2f40d68460f7831088a6d2cda31fa994f45395c6
peter4tech <peterbozz1996@gmail.com>
2026-04-10 09:59:45 +0100

    Ui-fix-redevelop

--- src/shared/utils/ScrollHandler.tsx
+++
@@ -1,8 +0,0 @@
-// components/utils/ScrollHandler.tsx
-'use client';
-import { useScrollToTopOnPageChange } from './hooks/useScroll';
-
-export default function ScrollHandler() {
-  useScrollToTopOnPageChange();
-  return null;
-}
