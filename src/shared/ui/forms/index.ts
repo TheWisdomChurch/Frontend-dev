0505304d6e6dc04f27ffb9da5bb2c9a7fddecb86
peter4tech <34604032+peterchims@users.noreply.github.com>
2026-09-03 20:57:58 +0100

    fixes-form-ongoing

--- src/shared/ui/forms/index.ts
+++ src/shared/ui/forms/index.ts
@@ -1,2 +1,42 @@
 export { PhoneNumberField } from './PhoneNumberField';
 export { LocationFields, type LocationValue } from './LocationFields';
+
+export {
+  Field,
+  controlClass,
+  controlErrorClass,
+  fieldLabelClass,
+  fieldHintClass,
+  fieldHelpClass,
+  type FieldProps,
+} from './Field';
+
+export {
+  TextField,
+  TextareaField,
+  SelectField,
+  RadioGroupField,
+  CheckboxGroupField,
+  CheckboxField,
+  PhoneField,
+  DateField,
+  type FieldControlProps,
+} from './fields';
+
+export {
+  ImageField,
+  isImageFieldValue,
+  imageFieldIsBusy,
+  imageFieldHasBlockingError,
+  resolveImageSubmissionValue,
+  type ImageFieldValue,
+  type ImageFieldProps,
+} from './ImageField';
+
+export { FormShell, type FormShellProps } from './FormShell';
+export {
+  ConsentDisclosure,
+  type ConsentContent,
+  type ConsentDisclosureProps,
+} from './ConsentDisclosure';
+export { FormSuccess, type FormSuccessProps } from './FormSuccess';
